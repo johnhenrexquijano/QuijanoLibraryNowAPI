2e90dbd2c66944906cc48ed55656bf8719899b8e
STUDENT 18 <STUDENT 18@DESKTOP-R74NTU9>
2026-05-09 17:28:56 +0800

    push

---
+++ QuijanoLibraryNowAPI/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace QuijanoLibraryNowAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String? Title { get; set; }
+        public String? Author { get; set; }
+        public String? Genre { get; set; }
+        public bool Available { get; set; }
+        public int PublishedYear { get; set; }
+    }
+}
+
+
